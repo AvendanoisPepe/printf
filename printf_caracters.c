6011bb44ad1105c2c7614b59be6c0900c2931c07
Andres.Pardo <carlos.pardo@uptc.edu.co>
2021-11-07 12:40:33 -0500

    funcion de caracter y funcion para cadena

--- printf_caracters.c
+++ printf_caracters.c
@@ -1,9 +1,35 @@
 #include "main.c"
 /**
- * 
- * 
+ * printfc: la funcion lo que hace es imprimir un caracter de salida 
+ * @lista: lista de argumentos 
+ * @guar: longitud de cadena 
+ * Return : guar 
  */
-int printfc(va_list lista, int guar)
+int printfc(va_list lista, int guar) 
 {
 	char ca = va_arg(lista, int);
+	guar = guar +_putchar(ca);
+	return (guar);
 }
+/**
+ * printfs: la funcion lo que hace es imprimir una cadena de salida
+ * @lista: lista de argumentos 
+ * @guar: longitud de la cadena
+ * Return: guar
+ */
+int printfs(va_list lista, int guar)
+{
+	char *st = va_arg(lista, char *); 
+	if (st == NULL)
+	{
+	  st = "(Null)";
+	}
+	while(*st != NULL)
+	{
+		guar  = guar + _putchar(*st++);
+	}
+	return (guar);
+
+}
+
+
