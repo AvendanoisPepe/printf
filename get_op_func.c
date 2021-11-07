dc58d6d1dcb19efed3406d6bf8e36a9040a3f9b0
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-11-07 03:58:50 +0000

    Creamos la estructura base

---
+++ get_op_func.c
@@ -0,0 +1,9 @@
+#include "math.h"
+#include <stdarg.h>
+/**
+ * 
+ */
+int (*get_op_func(char s))(va_list, int)
+{
+	
+}
--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+
+#include <stdarg.h>
+
 int _putchar(char c);
 int _printf(const char *format, ...);
+
+/**
+ * struct main - Struct op
+ *
+ * @moduladores: The operator
+ * @modul: The function associated
+ */
+typedef struct main
+{
+    char *moduladores;
+    int (*funt)(va_list, int);
+} modul;
+
 #endif
