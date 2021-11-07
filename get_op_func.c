105426dc3dc0ba4784375ccaaf74ba9ce3eebb8f
Andres.Pardo <carlos.pardo@uptc.edu.co>
2021-11-06 23:46:33 -0500

    creando array unidimencional que contiene los moduladores

--- get_op_func.c
+++ get_op_func.c
@@ -1,9 +1,28 @@
-#include "math.h"
+#include "main.c"
 #include <stdarg.h>
+#include <string.h>
 /**
- * 
+ *  get_op_func - selects the correct operation toperform
+ * @s: operator
+ * Return: pointer to the correct function
  */
 int (*get_op_func(char s))(va_list, int)
 {
-	
+	modul valid[] = 
+    {
+        {"c", printfc},
+        {"s", printfs},
+        {"d", printfd},
+        {"i", printfi},
+        {NULL, NULL}
+    };
+
+    int iterador;
+    for (iterador = 0; valid[iterador].moduladores; iterador++)
+    {
+        if (strcmp(valid[iterador].moduladores, s) == 0)
+        return (valid[iterador].funt);
+    }
+    return (NULL);
+
 }
