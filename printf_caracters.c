b5312718d15688154b63b10f1b27125ba337ef1c
AvendanoisPepe <jsavendano62@misena.edu.co>
2021-11-08 18:51:16 +0000

    Correciones de betty en prinf_caracters

--- printf_caracters.c
+++ printf_caracters.c
@@ -1,25 +1,27 @@
 #include "main.h"
 /**
- * printfc: la funcion lo que hace es imprimir un caracter de salida 
- * @lista: lista de argumentos 
- * @guar: longitud de cadena 
- * Return : guar 
+ * printfc - la funcion lo que hace es imprimir un caracter de salida
+ * @lista: lista de argumentos
+ * @guar: longitud de cadena
+ * Return: guar
  */
 int printfc(va_list lista, int guar)
 {
 	char ca = va_arg(lista, int);
+
 	guar = guar + _putchar(ca);
 	return (guar);
 }
 /**
- * printfs: la funcion lo que hace es imprimir una cadena de salida
- * @lista: lista de argumentos 
+ * printfs - la funcion lo que hace es imprimir una cadena de salida
+ * @lista: lista de argumentos
  * @guar: longitud de la cadena
  * Return: guar
  */
 int printfs(va_list lista, int guar)
 {
 	char *st = va_arg(lista, char *);
+
 	if (st == NULL)
 	{
 		st = "(Null)";
